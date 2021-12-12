6b80a276ccc5c7d0df72cea7a6c87e010831b193
limoncellio <81052585+ychenuva@users.noreply.github.com>
2021-12-12 22:38:10 +0100

    Update discriminator.py

--- architectures/discriminator.py
+++ architectures/discriminator.py
@@ -1,4 +1,5 @@
 """
+Code adapted from https://github.com/NVlabs/SPADE
 Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
@@ -6,42 +7,63 @@ Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses
 import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
-from models.networks.base_network import BaseNetwork
-from models.networks.normalization import get_nonspade_norm_layer
-import util.util as util
+from architectures.SPADE import BaseNetwork
+from synchronised_batch_norm.batchnorm import SynchronizedBatchNorm2d
+import torch.nn.utils.spectral_norm as spectral_norm
+
+
+
+# Returns a function that creates a normalization function
+# that does not condition on semantic map
+def get_nonspade_norm_layer(norm_type='instance'):
+    # helper function to get # output channels of the previous layer
+    def get_out_channel(layer):
+        if hasattr(layer, 'out_channels'):
+            return getattr(layer, 'out_channels')
+        return layer.weight.size(0)
+
+    # this function will be returned
+    def add_norm_layer(layer):
+        nonlocal norm_type
+        if norm_type.startswith('spectral'):
+            layer = spectral_norm(layer)
+            subnorm_type = norm_type[len('spectral'):]
+
+        if subnorm_type == 'none' or len(subnorm_type) == 0:
+            return layer
+
+        # remove bias in the previous layer, which is meaningless
+        # since it has no effect after normalization
+        if getattr(layer, 'bias', None) is not None:
+            delattr(layer, 'bias')
+            layer.register_parameter('bias', None)
+
+        if subnorm_type == 'batch':
+            norm_layer = nn.BatchNorm2d(get_out_channel(layer), affine=True)
+        elif subnorm_type == 'sync_batch':
+            norm_layer = SynchronizedBatchNorm2d(get_out_channel(layer), affine=True)
+        elif subnorm_type == 'instance':
+            norm_layer = nn.InstanceNorm2d(get_out_channel(layer), affine=False)
+        else:
+            raise ValueError('normalization layer %s is not recognized' % subnorm_type)
+
+        return nn.Sequential(layer, norm_layer)
+
+    return add_norm_layer
 
 
 class MultiscaleDiscriminator(BaseNetwork):
-    @staticmethod
-    def modify_commandline_options(parser, is_train):
-        parser.add_argument('--netD_subarch', type=str, default='n_layer',
-                            help='architecture of each discriminator')
-        parser.add_argument('--num_D', type=int, default=2,
-                            help='number of discriminators to be used in multiscale')
-        opt, _ = parser.parse_known_args()
 
-        # define properties of each discriminator of the multiscale discriminator
-        subnetD = util.find_class_in_module(opt.netD_subarch + 'discriminator',
-                                            'models.networks.discriminator')
-        subnetD.modify_commandline_options(parser, is_train)
 
-        return parser
-
-    def __init__(self, opt):
+    def __init__(self):
         super().__init__()
-        self.opt = opt
-
-        for i in range(opt.num_D):
-            subnetD = self.create_single_discriminator(opt)
+        num_D = 2
+        for i in range(num_D):
+            subnetD = self.create_single_discriminator()
             self.add_module('discriminator_%d' % i, subnetD)
 
-    def create_single_discriminator(self, opt):
-        subarch = opt.netD_subarch
-        if subarch == 'n_layer':
-            netD = NLayerDiscriminator(opt)
-        else:
-            raise ValueError('unrecognized discriminator subarchitecture %s' % subarch)
-        return netD
+    def create_single_discriminator(self):
+        netD = NLayerDiscriminator()
 
     def downsample(self, input):
         return F.avg_pool2d(input, kernel_size=3,
@@ -52,7 +74,7 @@ class MultiscaleDiscriminator(BaseNetwork):
     # The final result is of size opt.num_D x opt.n_layers_D
     def forward(self, input):
         result = []
-        get_intermediate_features = not self.opt.no_ganFeat_loss
+        get_intermediate_features = True
         for name, D in self.named_children():
             out = D(input)
             if not get_intermediate_features:
@@ -65,29 +87,24 @@ class MultiscaleDiscriminator(BaseNetwork):
 
 # Defines the PatchGAN discriminator with the specified arguments.
 class NLayerDiscriminator(BaseNetwork):
-    @staticmethod
-    def modify_commandline_options(parser, is_train):
-        parser.add_argument('--n_layers_D', type=int, default=4,
-                            help='# layers in each discriminator')
-        return parser
 
-    def __init__(self, opt):
+    def __init__(self):
         super().__init__()
-        self.opt = opt
 
         kw = 4
         padw = int(np.ceil((kw - 1.0) / 2))
-        nf = opt.ndf
-        input_nc = self.compute_D_input_nc(opt)
+        nf = 64
+		n_layers_D = 3 # todo parser default says 4 but issue/36 says 3 
+        input_nc = self.compute_D_input_nc()
 
-        norm_layer = get_nonspade_norm_layer(opt, opt.norm_D)
+        norm_layer = get_nonspade_norm_layer('spectralinstance')
         sequence = [[nn.Conv2d(input_nc, nf, kernel_size=kw, stride=2, padding=padw),
                      nn.LeakyReLU(0.2, False)]]
 
-        for n in range(1, opt.n_layers_D):
+        for n in range(1, n_layers_D):
             nf_prev = nf
             nf = min(nf * 2, 512)
-            stride = 1 if n == opt.n_layers_D - 1 else 2
+            stride = 1 if n == n_layers_D - 1 else 2
             sequence += [[norm_layer(nn.Conv2d(nf_prev, nf, kernel_size=kw,
                                                stride=stride, padding=padw)),
                           nn.LeakyReLU(0.2, False)
@@ -99,11 +116,16 @@ class NLayerDiscriminator(BaseNetwork):
         for n in range(len(sequence)):
             self.add_module('model' + str(n), nn.Sequential(*sequence[n]))
 
-    def compute_D_input_nc(self, opt):
-        input_nc = opt.label_nc + opt.output_nc
-        if opt.contain_dontcare_label:
+    def compute_D_input_nc(self):
+	    label_nc = 182
+		output_nc = 3
+		contain_dontcare_label = True
+		no_instance = False
+		
+        input_nc = label_nc + output_nc
+        if contain_dontcare_label:
             input_nc += 1
-        if not opt.no_instance:
+        if not no_instance:
             input_nc += 1
         return input_nc
 
@@ -113,7 +135,7 @@ class NLayerDiscriminator(BaseNetwork):
             intermediate_output = submodel(results[-1])
             results.append(intermediate_output)
 
-        get_intermediate_features = not self.opt.no_ganFeat_loss
+        get_intermediate_features = True
         if get_intermediate_features:
             return results[1:]
         else:
