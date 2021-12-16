c4db52d2a3a2ef4e4b4511d324a820b40d44d2a9
Morpheus3000 <morpheus.morphia@gmail.com>
2021-12-16 18:57:05 +0100

    Bug fixes for gpu and cpu memory issues.

--- Trainer.py
+++ Trainer.py
@@ -11,13 +11,13 @@ from torch.utils.data import DataLoader
 
 from Network import EdgeGuidedNetwork
 from DataLoader import CityscapesDataset
-from Criterions import MultiModalityDiscriminatorLoss
+from Criterions import MultiModalityDiscriminatorLoss as crit
 from Utils import mor_utils
 from architectures.discriminator import MultiscaleDiscriminator as discriminator
 
+
 torch.backends.cudnn.benchmark = True
 
-# TODO: Set up GAN training schedule
 
 cudaDevice = ''
 
@@ -40,22 +40,20 @@ outdir = 'OutputJpeg%s' % ExperimentName.replace(' ', '_')
 os.makedirs(outdir, exist_ok=True)
 
 modelSaveLoc = outdir + '/snapshot_%d.t7'
-logTrain = '%s/LogTrain.str' % outdir
-logTest = '%s/LogTest.str' % outdir
 
-data_root = '/ssdstore///v4/Processed/Intrinsics/'
-train_list = '/home//Experiments//v4/train_files.txt'
-test_list = '/home//Experiments//v4/test_files.txt'
+data_root = 'D:/Datasets/CityScapes/Cityscapes_train_full/'
+train_list = 'D:/Datasets/CityScapes/Cityscapes_train_full/train_list.str'
+# test_list = '/home//Experiments//v4/test_files.txt'
 
 seg_classes = 34
-batch_size = 4
+batch_size = 1
 nthreads = 4
 if batch_size < nthreads:
     nthreads = batch_size
 max_epochs = 200 # 250
 lr_mod_epoch = 100
 displayIter = 10
-saveIter = 50000
+# saveIter = 50000
 
 lambda_gan = 1
 lambda_feat = 10
@@ -80,43 +78,29 @@ if torch.cuda.device_count() > 1:
     net = nn.DataParallel(net)
 net.to(device)
 
-edge_discriminator = discriminator(in_channels=(seg_classes + 1)).to(device)
-edge_discriminator.init_weights('xavier', 0.02)
+discrim = discriminator(in_channels=(seg_classes + 3)).to(device)
+discrim.init_weights('xavier', 0.02)
 if torch.cuda.device_count() > 1:
     print("Let's use", torch.cuda.device_count(), "GPUs!...", end='')
-    edge_discriminator = nn.DataParallel(edge_discriminator)
-edge_discriminator.to(device)
-
-image_discriminator = discriminator(in_channels=(seg_classes + 3)).to(device)
-image_discriminator.init_weights('xavier', 0.02)
-if torch.cuda.device_count() > 1:
-    print("Let's use", torch.cuda.device_count(), "GPUs!...", end='')
-    image_discriminator = nn.DataParallel(image_discriminator)
-image_discriminator.to(device)
+    discrim = nn.DataParallel(discrim)
+discrim.to(device)
 
 
 print(done)
 print('[I] STATUS: Initiate optimizer...', end='')
 optimizer_G = torch.optim.Adam(net.parameters(), lr=learningRate / 2, betas=(beta_1, beta_2))
-optimizer_D_e = torch.optim.Adam(edge_discriminator.parameters(), lr=learningRate / 2, betas=(beta_1, beta_2))
-optimizer_D_i = torch.optim.Adam(image_discriminator.parameters(), lr=learningRate / 2, betas=(beta_1, beta_2))
+optimizer_D = torch.optim.Adam(discrim.parameters(), lr=learningRate / 2, betas=(beta_1, beta_2))
 scheduler_G = torch.optim.lr_scheduler.LinearLR(optimizer_G, start_factor=1,
                                               end_factor=0, total_iters=100,
                                               verbose=True)
-scheduler_D_e = torch.optim.lr_scheduler.LinearLR(optimizer_D_e, start_factor=1,
+scheduler_D = torch.optim.lr_scheduler.LinearLR(optimizer_D, start_factor=1,
                                               end_factor=0, total_iters=100,
                                               verbose=True)
-
-scheduler_D_i = torch.optim.lr_scheduler.LinearLR(optimizer_D_i, start_factor=1,
-                                              end_factor=0, total_iters=100,
-                                              verbose=True)
-
 
 print(done)
 print('[I] STATUS: Initiate Criterions and transfer to device...', end='')
-criterion = MultiModalityDiscriminator(
-    edge_discriminator=edge_discriminator,
-    image_discriminator=image_discriminator,
+criterion = crit(
+    discrim,
     seg_classes=seg_classes, lambda_feat=lambda_feat, lambda_gan=lambda_gan,
     lambda_vgg=lambda_vgg, lambd=lambd
 ).to(device)
@@ -189,17 +173,25 @@ def Train(net, epoch_count):
 
         # rgb = Variable(images[0]).to(device)
         seg = Variable(images['sem']).to(device)
+        rgb = Variable(images['rgb']).to(device)
+        edge = Variable(images['edge']).to(device)
+        edge = edge.expand(-1, 3, -1, -1)
 
         optimizer_G.zero_grad()
-        optimizer_D_e.zero_grad()
-        optimizer_D_i.zero_grad()
+        optimizer_D.zero_grad()
 
         net_time = time.time()
         pred = net(seg)
 
+        target = {
+            'sem': seg,
+            'rgb': rgb,
+            'edge': edge
+        }
+
         # Update Generator
 
-        total_loss_G, ret_pack_G = criterion(pred, images, update='generator')
+        total_loss_G, ret_pack_G = criterion(pred, target, update='generator')
         G_loss = ret_pack_G[0]
         img_G_loss = ret_pack_G[1]
 
@@ -207,25 +199,24 @@ def Train(net, epoch_count):
         optimizer_G.step()
 
         edge_loss_G[i] = lambda_gan * G_loss['GAN'].cpu().detach().numpy()
-        edge_loss_feat[i] = lambda_feat * img_G_loss['GAN_Feat'].cpu().detach().numpy()
-        edge_loss_percep[i] = lambda_vgg * img_G_loss['VGG'].cpu().detach().numpy()
+        edge_loss_feat[i] = lambda_feat * G_loss['GAN_Feat'].cpu().detach().numpy()
+        edge_loss_percep[i] = lambda_vgg * G_loss['VGG'].cpu().detach().numpy()
 
-        img_d_loss_G[i] = lambda_gan * G_loss['GAN_1'].cpu().detach().numpy()
+        img_d_loss_G[i] = lambda_gan * img_G_loss['GAN_1'].cpu().detach().numpy()
         img_d_loss_feat[i] = lambda_feat * img_G_loss['GAN_Feat_1'].cpu().detach().numpy()
         img_d_loss_percep[i] = lambda_vgg * img_G_loss['VGG_1'].cpu().detach().numpy()
 
-        img_dd_loss_G[i] = lambda_gan * G_loss['GAN_2'].cpu().detach().numpy()
+        img_dd_loss_G[i] = lambda_gan * img_G_loss['GAN_2'].cpu().detach().numpy()
         img_dd_loss_feat[i] = lambda_feat * img_G_loss['GAN_Feat_2'].cpu().detach().numpy()
         img_dd_loss_percep[i] = lambda_vgg * img_G_loss['VGG_2'].cpu().detach().numpy()
 
         # Update Discriminator
-        total_loss_D, ret_pack_D = criterion(pred, images, update='discriminator')
+        total_loss_D, ret_pack_D = criterion(pred, target, update='discriminator')
         D_loss = ret_pack_D[0]
         img_D_loss = ret_pack_D[1]
 
         total_loss_D.backward()
-        optimizer_D_e.step()
-        optimizer_D_i.step()
+        optimizer_D.step()
 
         edge_loss_D[i] = D_loss['D_Fake'].cpu().detach().numpy() +\
             D_loss['D_real'].cpu().detach().numpy()
@@ -235,38 +226,17 @@ def Train(net, epoch_count):
 
         net_timed = time.time() - net_time
 
-        if iter_count % saveIter == 0:
-            support.saveModels(
-                {'G': net,
-                 'D_e': edge_discriminator,
-                 'D_i': image_discriminator},
-                {'G': optimizer_G,
-                 'D_e': optimizer_D_e,
-                 'D_i': optimizer_D_i},
-                iter_count,
-                modelSaveLoc % iter_count)
-
-            tqdm.write('[!] Model Saved!')
-
-        t.set_description('[Iter %d] Gen_e: %0.4f, Feat_e: %0.4f, Percep_e: %0.4f,'
-                          ' Gen_i_d: %0.4f, Feat_i_d: %0.4f, Percep_i_d: %0.4f,'
-                          ' Gen_i_dd: %0.4f, Feat_i_dd: %0.4f, Percep_i_dd: %0.4f,'
-                          ' D_e: %0.4f, D_i: %0.4f, Epoch: %d, Time: %0.4f' % (
+        # if iter_count % saveIter == 0:
+        t.set_description('[Iter %d] Feat_e: %0.4f, Percep_e: %0.4f,'
+                          ' Feat_i_dd: %0.4f, Percep_i_dd: %0.4f,'
+                          ' Epoch: %d, Time: %0.4f' % (
                               iter_count,
-                              edge_loss_G[i],
                               edge_loss_feat[i],
                               edge_loss_percep[i],
 
-                              img_d_loss_G[i],
-                              img_d_loss_feat[i],
-                              img_d_loss_percep[i],
-
-                              img_dd_loss_G[i],
                               img_dd_loss_feat[i],
                               img_dd_loss_percep[i],
 
-                              edge_loss_D[i],
-                              img_loss_D[i],
                               epoch_count, net_timed
                           ))
 
@@ -286,13 +256,23 @@ def Train(net, epoch_count):
     avg_i_D_loss = np.nanmean(img_loss_D)
 
     Epoch_timed = time.time() - Epoch_time
+
+    support.saveModels(
+        {'G': net,
+         'D': discrim},
+        {'G': optimizer_G,
+         'D': optimizer_D},
+        iter_count,
+        modelSaveLoc % iter_count)
+
+    print('[!] Model Saved!')
+
     print('[I] STATUS: Exp: %s: Epoch %d trained! Time taken: %0.2f minutes' %
           (ExperimentName, epoch_count, Epoch_timed / 60))
 
     if epoch_count >= lr_mod_epoch - 1:
         scheduler_G.step()
-        scheduler_D_e.step()
-        scheduler_D_i.step()
+        scheduler_D.step()
 
     return avg_g_e_loss,\
         avg_f_e_loss,\
@@ -307,52 +287,56 @@ def Train(net, epoch_count):
         avg_i_D_loss
 
 
-iter_count = 0
+def main():
+    global iter_count
 
-print('[*] Beginning Training:')
-print('\tMax Epoch: ', max_epochs)
-print('\tLogging iter: ', displayIter)
-print('\tSaving iter: ', saveIter)
-print('\tModels Dumped at: ', outdir)
-print('\tExperiment Name: ', ExperimentName)
+    iter_count = 0
 
-for i in range(max_epochs):
-    avg_g_e_loss,\
-        avg_f_e_loss,\
-        avg_p_e_loss,\
-        avg_g_d_loss,\
-        avg_f_d_loss,\
-        avg_p_d_loss,\
-        avg_g_dd_loss,\
-        avg_f_dd_loss,\
-        avg_p_dd_loss,\
-        avg_e_D_loss,\
-        avg_i_D_loss = Train(net, i)
+    print('[*] Beginning Training:')
+    print('\tMax Epoch: ', max_epochs)
+    print('\tLogging iter: ', displayIter)
+    print('\tModels Dumped at: ', outdir)
+    print('\tExperiment Name: ', ExperimentName)
 
-    print('[*] Epoch %d - Gen_e: %0.4f, Feat_e: %0.4f, Percep_e: %0.4f,'
-          ' Gen_i_d: %0.4f, Feat_i_d: %0.4f, Percep_i_d: %0.4f,'
-          ' Gen_i_dd: %0.4f, Feat_i_dd: %0.4f, Percep_i_dd: %0.4f,'
-          ' D_e: %0.4f, D_i: %0.4f,' % (
-              i + 1,
-              avg_g_e_loss,
-              avg_f_e_loss,
-              avg_p_e_loss,
-              avg_g_d_loss,
-              avg_f_d_loss,
-              avg_p_d_loss,
-              avg_g_dd_loss,
-              avg_f_dd_loss,
-              avg_p_dd_loss,
-              avg_e_D_loss,
-              avg_i_D_loss
-          ))
+    for i in range(max_epochs):
+        avg_g_e_loss,\
+            avg_f_e_loss,\
+            avg_p_e_loss,\
+            avg_g_d_loss,\
+            avg_f_d_loss,\
+            avg_p_d_loss,\
+            avg_g_dd_loss,\
+            avg_f_dd_loss,\
+            avg_p_dd_loss,\
+            avg_e_D_loss,\
+            avg_i_D_loss = Train(net, i)
 
-    support.saveModels(
-        {'G': net,
-         'D_e': edge_discriminator,
-         'D_i': image_discriminator},
-        {'G': optimizer_G,
-         'D_e': optimizer_D_e,
-         'D_i': optimizer_D_i},
-        iter_count,
-        modelSaveLoc % iter_count)
+        print('[*] Epoch %d - Gen_e: %0.4f, Feat_e: %0.4f, Percep_e: %0.4f,'
+              ' Gen_i_d: %0.4f, Feat_i_d: %0.4f, Percep_i_d: %0.4f,'
+              ' Gen_i_dd: %0.4f, Feat_i_dd: %0.4f, Percep_i_dd: %0.4f,'
+              ' D_e: %0.4f, D_i: %0.4f,' % (
+                  i + 1,
+                  avg_g_e_loss,
+                  avg_f_e_loss,
+                  avg_p_e_loss,
+                  avg_g_d_loss,
+                  avg_f_d_loss,
+                  avg_p_d_loss,
+                  avg_g_dd_loss,
+                  avg_f_dd_loss,
+                  avg_p_dd_loss,
+                  avg_e_D_loss,
+                  avg_i_D_loss
+              ))
+
+        support.saveModels(
+            {'G': net,
+             'D': discrim},
+            {'G': optimizer_G,
+             'D': optimizer_D},
+            iter_count,
+            modelSaveLoc % iter_count)
+
+
+if __name__ == '__main__':
+    main()
